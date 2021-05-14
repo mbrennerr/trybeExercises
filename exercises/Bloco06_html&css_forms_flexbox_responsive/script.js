646986e03f92613ae9043e845457a503b2b9f941
brennerr <marciobrennerbusiness@gmail.com>
2021-05-14 13:22:19 -0400

    Adiciona Exercício 6-1

--- exercises/Bloco06_html&css_forms_flexbox_responsive/script.js
+++ exercises/Bloco06_html&css_forms_flexbox_responsive/script.js
@@ -1,44 +1,15 @@
 // Chama o Estado;
-let inputEstado = document.getElementById("estado");
-let todosEstados = [
-['Acre', 'AC'],
-  ['Alagoas', 'AL'],
-  ['Amapá', 'AP'],
-  ['Amazonas', 'AM'],
-  ['Bahia', 'BA'],
-  ['Ceará', 'CE'],
-  ['Distrito Federal', 'DF'],
-  ['Espírito Santo', 'ES'],
-  ['Goiás', 'GO'],
-  ['Maranhão', 'MA'],
-  ['Mato Grosso', 'MT'],
-  ['Mato Grosso do Sul', 'MS'],
-  ['Minas Gerais', 'MG'],
-  ['Pará', 'PA'],
-  ['Paraíba', 'PB'],
-  ['Paraná', 'PR'],
-  ['Pernambuco', 'PE'],
-  ['Piauí', 'PI'],
-  ['Rio de Janeiro', 'RJ'],
-  ['Rio Grande do Norte', 'RN'],
-  ['Rio Grande do Sul', 'RS'],
-  ['Rondônia', 'RO'],
-  ['Roraima', 'RR'],
-  ['Santa Catarina', 'SC'],
-  ['São Paulo', 'SP'],
-  ['Sergipe', 'SE'],
-  ['Tocantins', 'TO']
-];
-function criarEstados() {
-    for (let i = 0; i < todosEstados.length; i += 1) {
-      const novoEstado = document.createElement('option');
-      novoEstado.innerText = todosEstados[i][0];
-      novoEstado.value = todosEstados[i][1];
-      inputEstado.appendChild(novoEstado);
-    }
+function createStateOptions() {
+  let states = document.getElementById('state');
+  let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+
+  for (let index = 0; index < stateOptions.length; index ++) {
+    let createOptions = document.createElement('option');
+    states.appendChild(createOptions).innerText = stateOptions[index];
+    states.appendChild(createOptions).value = stateOptions[index];
   }
-  window.onload = function () {
-    criarEstados();
+}
+createStateOptions();
 
 
 
